ccb810c35da7b5318cb33b132586eac65c4ac429
chenshun00 <chenshun00@gmail.com>
2021-08-23 09:16:06 +0800

    :sparkles: btree insert(not complete)

---
+++ src/main/java/io/github/chenshun00/data/tree/BTree.java
@@ -0,0 +1,340 @@
+package io.github.chenshun00.data.tree;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * <a href="https://stackoverflow.com/questions/5890960/print-btree-by-level">print-btree-by-level</a>
+ *
+ * @author luobo.cs@raycloud.com
+ * @since 2021/8/22 7:51 上午
+ */
+public class BTree {
+
+    public static void main(String[] args) {
+        BTree bTree = new BTree(3);
+        bTree.insert(1200);
+        bTree.insert(1300);
+        bTree.insert(1400);
+        bTree.insert(1500);
+        bTree.insert(1600);
+        bTree.insert(1700);
+        bTree.insert(1800);
+        bTree.insert(1900);
+        bTree.insert(2000);
+        bTree.insert(2100);
+        bTree.insert(2200);
+//        bTree.insert(2300);
+//        bTree.insert(2400);
+//        bTree.insert(2500);
+//        bTree.insert(2600);
+//        bTree.insert(2700);
+//        bTree.insert(2800);
+//        bTree.insert(2900);
+//        bTree.insert(3000);
+        bTree.traverse();
+    }
+
+    //数据和孩子
+    // m/2<=数据<=m-1
+    // m/2<=孩子<=m-1
+    private final int m;
+
+    private Node root;
+    private int height;
+
+    public BTree(int m) {
+        this.m = m;
+    }
+
+    public void insert(int data) {
+        //如果root节点为空
+        if (root == null) {
+            root = new Node();
+            root.setData(data);
+            return;
+        }
+        //找到数据节点
+        final Node node = findNode(root, data);
+        final int count = node.setData(data);
+        if (count <= m - 1) {
+            return;
+        }
+        //节点分裂
+        split(node, node == root);
+    }
+
+    private void split(Node node, boolean isRoot) {
+        //1、选出中间节点 ， 这里分为偶数和奇数，奇数取中间值. 偶数取中间值-1
+        int childIndex = (int) (m % 2 == 0 ? (Math.ceil(m >> 1) - 1) : Math.ceil(m >> 1));
+        final int[] key = node.key;
+        //上移节点的数据
+        final int indexValue = key[childIndex];
+        //root节点需要进行抽离
+        if (isRoot) {
+
+
+            Node newRoot = new Node();
+            newRoot.setData(indexValue);
+            root = newRoot;
+
+            //当切节点分裂成2个节点
+            //做节点
+            Node leftChild = new Node();
+            leftChild.key = new int[childIndex];
+            System.arraycopy(node.key, 0, leftChild.key, 0, childIndex);
+            leftChild.trim();
+            leftChild.parent = root;
+            //右节点
+            Node rightChild = new Node();
+            rightChild.key = new int[key.length - childIndex - 1];
+            System.arraycopy(node.key, childIndex + 1, rightChild.key, 0, key.length - childIndex - 1);
+            rightChild.trim();
+            rightChild.parent = root;
+
+            root.trim();
+            //修改新节点
+            root.setChild(0, leftChild);
+            root.setChild(1, rightChild);
+
+            final Node[] children = node.children;
+            if (children != null) {
+                //进行分裂
+                leftChild.children = new Node[childIndex + 1];
+                //进行拷贝
+                System.arraycopy(children, 0, leftChild.children, 0, childIndex + 1);
+                for (Node child : leftChild.children) {
+                    child.parent = leftChild;
+                }
+                rightChild.children = new Node[children.length - childIndex - 1];
+                System.arraycopy(children, childIndex + 1, rightChild.children, 0, children.length - childIndex - 1);
+                for (Node child : rightChild.children) {
+                    child.parent = rightChild;
+                }
+            }
+        } else {
+            //             2000(A)
+            //            /     \
+            // [750 1000 500]B  [3000]C
+            // 当前Node=B
+            //获取当前叶子节点的parent节点 A
+            final Node parent = node.parent;
+            //            1000,2000(A)
+            //            /     \
+            // [750 1000 500]B  [3000]C
+            parent.setData(indexValue);
+
+            //当切节点分裂成2个节点
+            //做节点
+            Node leftChild = new Node();
+            leftChild.key = new int[childIndex];
+            System.arraycopy(node.key, 0, leftChild.key, 0, childIndex);
+            leftChild.trim();
+            leftChild.parent = parent;
+            //右节点
+            Node rightChild = new Node();
+            rightChild.key = new int[key.length - childIndex - 1];
+            System.arraycopy(node.key, childIndex + 1, rightChild.key, 0, key.length - childIndex - 1);
+            rightChild.trim();
+            rightChild.parent = parent;
+
+            var children = parent.children;
+            int parentChildIndex = -1;
+            for (int i = 0; i < children.length; i++) {
+                if (children[i] == node) {
+                    parentChildIndex = i;
+                    break;
+                }
+            }
+
+            var tempChildren = new Node[children.length + 1];
+            System.arraycopy(children, 0, tempChildren, 0, parentChildIndex);
+            System.arraycopy(children, parentChildIndex + 1, tempChildren, parentChildIndex + 2, children.length - parentChildIndex - 1);
+            parent.children = tempChildren;
+            parent.children[parentChildIndex] = leftChild;
+            parent.children[parentChildIndex + 1] = rightChild;
+            parent.trim();
+
+            final Node[] nodeChildren = node.children;
+            if (nodeChildren != null) {
+                //进行分裂
+                leftChild.children = new Node[parentChildIndex + 1];
+                //进行拷贝
+                System.arraycopy(children, 0, leftChild.children, 0, parentChildIndex + 1);
+                rightChild.children = new Node[children.length - parentChildIndex - 1];
+                System.arraycopy(children, parentChildIndex + 1, rightChild.children, 0, children.length - parentChildIndex - 1);
+            }
+            if (parent.key.length >= m) {
+                //在二次迭代中，需要将当前树的孩子的子树修改为新生成的子树的孩子
+                split(parent, parent == root);
+            }
+        }
+    }
+
+    //这里的递归也可以用 while(next != null){ next = next.child();}的形式来解决
+    private Node findNode(Node node, int data) {
+        //如果root是叶子节点
+        if (node.isLeaf()) {
+            return node;
+        }
+        //继续寻找叶子节点
+        //从root节点中寻找下继节点
+        final Node nextChild = nextChild(node, data);
+        return findNode(nextChild, data);
+    }
+
+    private Node nextChild(Node node, int data) {
+        final int min = node.getMin();
+        if (data <= min) {
+            return node.children[0];
+        }
+        final int max = node.getMax();
+        if (data >= max) {
+            return node.children[node.numberOfNodes];
+        }
+        for (int i = 1; i < node.key.length; i++) {
+            final int value = node.key[i];
+            if (data <= value) {
+                return node.children[i];
+            }
+        }
+        throw new IllegalStateException("不应该跑到这里");
+    }
+
+    public void traverse() {
+        if (root == null) {
+            System.out.println();
+            System.out.println("======");
+            return;
+        }
+        var queue = new LinkedBlockingQueue<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            try {
+                final Object take = queue.take();
+                if (take instanceof Node) {
+                    Node parent = (Node) take;
+                    doTraverse(parent);
+                    System.out.println();
+                    final Node[] children = parent.children;
+                    if (children == null) {
+                        continue;
+                    }
+                    queue.add(children);
+                } else {
+                    Node[] children = (Node[]) take;
+                    List<Node> nodes = new ArrayList<>();
+                    for (Node child : children) {
+                        doTraverse(child);
+                        if (child != null && child.children != null) {
+                            nodes.addAll(Arrays.asList(child.children));
+                        }
+                    }
+                    System.out.println();
+                    if (!nodes.isEmpty()) {
+                        queue.add(nodes.toArray(new Node[0]));
+                    }
+                }
+            } catch (InterruptedException ignore) {
+
+            }
+        }
+        System.out.println();
+        System.out.println("======");
+    }
+
+    private void doTraverse(Node node) {
+        if (node == null || node.key == null) {
+            return;
+        }
+        final int[] key = node.key;
+        final int level = maxLevel(node);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("     ".repeat(Math.max(0, level))).append("[");
+        for (int j : key) {
+            stringBuilder.append(j).append(",");
+        }
+        stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length());
+//        stringBuilder.append("(").append(node.isLeaf() ? "Y" : "N").append(")");
+        stringBuilder.append("]");
+        System.out.print(stringBuilder.toString());
+    }
+
+    private static <T extends Comparable<?>> int maxLevel(Node node) {
+        if (node == null)
+            return 0;
+
+        return maxLevel(node.children == null ? null : node.children.length <= 0 ? null : node.children[0]) + 1;
+    }
+
+    private final class Node {
+        public int numberOfNodes;
+        /**
+         * the array that holds the keys value.
+         */
+        public int key[];
+
+        /**
+         * 父亲节点
+         */
+        public Node parent;
+        /**
+         * the array that holds the references of the keys in the node.
+         */
+        public Node children[];
+
+        /**
+         * The constructor of the node class
+         * The node can have at most 3 keys. We have 4 references for each node, and assign the node to be isLeaf.
+         */
+        Node() {
+            numberOfNodes = 0;
+        }
+
+        public int setData(int data) {
+            int[] newKey = new int[numberOfNodes + 1];
+            if (key != null) {
+                System.arraycopy(key, 0, newKey, 0, key.length);
+            }
+            newKey[numberOfNodes] = data;
+            key = newKey;
+            numberOfNodes++;
+            //这里的排序是个问题，如何对key进行排序
+            Arrays.sort(key);
+            assert numberOfNodes < key.length;
+            return numberOfNodes;
+        }
+
+        public void setChild(int index, Node child) {
+            Node[] newChild = new Node[numberOfNodes + 1];
+            if (children != null) {
+                System.arraycopy(children, 0, newChild, 0, index);
+                newChild[index] = child;
+                System.arraycopy(children, index, newChild, index + 1, children.length - index - 1);
+            } else {
+                newChild[0] = child;
+            }
+            children = newChild;
+        }
+
+        public boolean isLeaf() {
+            return children == null;
+        }
+
+
+        public void trim() {
+            numberOfNodes = key == null ? 0 : key.length;
+        }
+
+        public int getMax() {
+            return key[numberOfNodes - 1];
+        }
+
+        public int getMin() {
+            return key[0];
+        }
+    }
+}
---
+++ src/test/java/io/github/chenshun00/data/test/CeilTest.java
@@ -0,0 +1,14 @@
+package io.github.chenshun00.data.test;
+
+/**
+ * @author luobo.cs@raycloud.com
+ * @since 2021/8/22 11:33 上午
+ */
+public class CeilTest {
+
+    public static void main(String[] args) {
+        final int ceil = (int) Math.ceil(4 / 2) -1;
+        System.out.println(ceil);
+    }
+
+}
