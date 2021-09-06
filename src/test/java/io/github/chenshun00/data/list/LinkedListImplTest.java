30e92e70d099447428a3a5cf97122d441e1da066
chenshun00 <chenshun00@gmail.com>
2021-09-06 09:21:47 +0800

    :white_check_mark: 添加测试代码

---
+++ src/test/java/io/github/chenshun00/data/list/LinkedListImplTest.java
@@ -0,0 +1,11 @@
+package io.github.chenshun00.data.list;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author luobo.cs@raycloud.com
+ * @since 2021/9/6 9:21 上午
+ */
+public class LinkedListImplTest {
+
+}
---
+++ src/test/java/io/github/chenshun00/data/stack/StackImplTest.java
@@ -0,0 +1,11 @@
+package io.github.chenshun00.data.stack;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author luobo.cs@raycloud.com
+ * @since 2021/9/6 9:21 上午
+ */
+public class StackImplTest {
+
+}
