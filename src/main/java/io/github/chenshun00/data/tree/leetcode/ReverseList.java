0985260ea2b9117bae8b32afe899ee95693de021
chenshun00 <chenshun00@gmail.com>
2022-01-09 11:29:54 +0800

    反转链表(真神奇呀)

--- src/main/java/io/github/chenshun00/data/tree/leetcode/ReverseList.java
+++ src/main/java/io/github/chenshun00/data/tree/leetcode/ReverseList.java
@@ -1,11 +1,15 @@
 package io.github.chenshun00.data.tree.leetcode;
 
+import java.util.Stack;
+
 /**
  * https://leetcode-cn.com/problems/reverse-linked-list/
  * <p>
  * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
  * <p>
  * 采用了最笨的办法实现的，需要仔细控制最后的分支条件，得画图理解
+ * <p>
+ * 绝的是，还可以利用Java的stack来实现
  *
  * @author chenshun00@gmail.com
  * @since 2022/1/8 11:17 下午
@@ -15,16 +19,69 @@ public class ReverseList {
     public static void main(String[] args) {
         ListNode listNode = new ListNode(1);
         ListNode p = listNode;
-        for (int i = 2; i <= 50; i++) {
+        for (int i = 2; i <= 6; i++) {
             p.next = new ListNode(i);
             p = p.next;
         }
         ReverseList reverseList = new ReverseList();
-        final ListNode xx = reverseList.reverseList(listNode);
-        xx.traverse();
+//        {
+//            final ListNode xx = reverseList.reverseList(listNode);
+//            xx.traverse();
+//        }
+//        {
+//            final ListNode xx = reverseList.quick(listNode);
+//            xx.traverse();
+//        }
+        {
+            final ListNode xx = reverseList.recursion(listNode);
+            xx.traverse();
+        }
+    }
+
+    /**
+     * 递归实现
+     * head=5.head.next=6
+     */
+    public ListNode recursion(ListNode head) {
+        if (head == null || head.next == null) return head;
+        //1-->2-->3-->4-->5-->6-->NULL
+        //6-->5-->4-->3-->2-->1-->NULL
+        //以前是1-->2
+        //t==1
+        //head=5. head.next=6
+        //last=6
+        final ListNode last = recursion(head.next);
+        //5.next=(6) 6.next=6;
+        head.next.next = head;
+        head.next = null;
+        return last;
     }
 
 
+    /**
+     * 巧妙利用Java栈实现
+     */
+    public ListNode quick(ListNode head) {
+        Stack<ListNode> listNodes = new Stack<>();
+        ListNode t = head;
+        while (t != null) {
+            listNodes.push(new ListNode(t.val));
+            t = t.next;
+        }
+        ListNode listNode = new ListNode();
+        ListNode tt = listNode;
+
+        while (!listNodes.isEmpty()) {
+            tt.next = listNodes.pop();
+            tt = tt.next;
+        }
+        return listNode.next;
+    }
+
+
+    /**
+     * 每次取出链表最后一个节点放到新链表上. 有些傻，颇有硬编码的意思
+     */
     public ListNode reverseList(ListNode head) {
         if (head == null) return null;
         if (head.next == null) return head;
