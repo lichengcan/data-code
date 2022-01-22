e7df2ab00538a02b31bd88fc0acdca7780609110
chenshun00 <chenshun00@gmail.com>
2022-01-22 16:17:54 +0800

    最长回文子串(暴力破解)，超时提交失败

--- src/main/java/io/github/chenshun00/data/other/t20220122/LengthOfLongestSubstring.java
+++ src/main/java/io/github/chenshun00/data/other/t20220122/LengthOfLongestSubstring.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 /**
+ * https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/comments/
+ *
  * @author chenshun00@gmail.com
  * @since 2022/1/22 3:36 下午
  */
@@ -12,6 +14,17 @@ public class LengthOfLongestSubstring {
     public static void main(String[] args) {
         LengthOfLongestSubstring lengthOfLongestSubstring = new LengthOfLongestSubstring();
         System.out.println(lengthOfLongestSubstring.lengthOfLongestSubstring("dvdf"));
+    }
+
+    /**
+     * a指针放0，b指针后移，出现重复，开始移动a指针到重复到地方.一直到结束
+     */
+    public int doublePoint(String s) {
+        int start = 0;
+        int end = 0;
+        final char[] chars = s.toCharArray();
+
+        return 0;
     }
 
     public int lengthOfLongestSubstring(String s) {
---
+++ src/main/java/io/github/chenshun00/data/other/t20220122/LongestPalindrome.java
@@ -0,0 +1,65 @@
+package io.github.chenshun00.data.other.t20220122;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author chenshun00@gmail.com
+ * @since 2022/1/22 4:00 下午
+ */
+public class LongestPalindrome {
+
+    public static void main(String[] args) {
+        LongestPalindrome longestPalindrome = new LongestPalindrome();
+        System.out.println(longestPalindrome.longestPalindrome("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth"));
+    }
+
+    Map<Integer, String> map = new HashMap<>();
+
+    public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
+        Integer integer = doLongestPalindrome(s);
+        return map.get(integer);
+    }
+
+    public Integer doLongestPalindrome(String s) {
+        if (s.length() <= 1) {
+            map.put(1, s);
+            return 1;
+        }
+        final int length = s.length();
+        int max = 1;
+        for (int i = 1; i <= length; i++) {
+            final String substring = s.substring(0, i);
+            if (isPalindrome(substring)) {
+                map.putIfAbsent(substring.length(), substring);
+                max = substring.length();
+            }
+        }
+        return Math.max(max, doLongestPalindrome(s.substring(1)));
+    }
+
+    /**
+     * 回文字符串
+     */
+    private boolean isPalindrome(String s) {
+        if (s.length() <= 1) {
+            return true;
+        }
+        final String reversal = reversal(s);
+        return reversal.equals(s);
+    }
+
+    /**
+     * 反转字符串
+     */
+    private String reversal(String s) {
+        if (s.length() == 1) {
+            return s;
+        }
+        return s.substring(s.length() - 1) + reversal(s.substring(0, s.length() - 1));
+    }
+
+}
